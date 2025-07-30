0c6fba185f998f85306c8bba10232752be860f7b
ashmita0shahi <ashmita0shahi@gmail.com>
2025-07-31 00:18:43 +0545

    SSL certificate and HTTPS done

--- web-bookit-frontend/vite.config.js
+++ web-bookit-frontend/vite.config.js
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import fs from 'fs';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+    server: {
+    https: {
+      key: fs.readFileSync('server.key'),
+      cert: fs.readFileSync('server.crt'),
+    },
+    port: 5173, // Your frontend port
+  },
 })
